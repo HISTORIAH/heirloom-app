02c16ab17dbfd1db063ad99324540883919c3346
Jimii <30603522+jim4067@users.noreply.github.com>
2026-04-18 21:26:39 +0300

    chore: try deployment (#17)
    * chore: try deployment
    
    * chore: add wrangler file

--- tests/claim.test.ts
+++ tests/claim.test.ts
@@ -26,26 +26,26 @@ test("it claims a native SOL vault", async () => {
   await sendClaim(client, { heir });
 });
 
-// test("it claims a token vault", async () => {
-//   const client = createDefaultSolanaClient();
-//   const authority = await loadDefaultKeypair();
+test("it claims a token vault", async () => {
+  const client = createDefaultSolanaClient();
+  const authority = await loadDefaultKeypair();
 
-//   const { mint } = await createAndMintTokens();
+  const { mint } = await createAndMintTokens();
 
-//   const { heir } = await sendInitialize(client, {
-//     amount: 1_000_000n,
-//     label: "test-claim-token",
-//     heartbeatInterval: 0n,
-//     gracePeriod: 0n,
-//     pauseDuration: 0n,
-//     mint: mint.address,
-//     tokenProgram: TOKEN_PROGRAM_ADDRESS,
-//   });
+  const { heir } = await sendInitialize(client, {
+    amount: 1_000_000n,
+    label: "test-claim-token",
+    heartbeatInterval: 0n,
+    gracePeriod: 0n,
+    pauseDuration: 0n,
+    mint: mint.address,
+    tokenProgram: TOKEN_PROGRAM_ADDRESS,
+  });
 
-//   // TODO: pass heirTokenAccount once ATA derivation is wired in sendClaim
-//   await sendClaim(client, {
-//     heir,
-//     mint: mint.address,
-//     tokenProgram: TOKEN_PROGRAM_ADDRESS,
-//   });
-// });
+  // TODO: pass heirTokenAccount once ATA derivation is wired in sendClaim
+  await sendClaim(client, {
+    heir,
+    mint: mint.address,
+    tokenProgram: TOKEN_PROGRAM_ADDRESS,
+  });
+});
